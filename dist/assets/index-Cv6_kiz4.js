4210303d260825f21039a2b8cfd69895aae22e43
javiermomc <javiermomc@outlook.com>
2026-07-14 18:27:54 -0600

    Add dist files

---
+++ dist/assets/index-Cv6_kiz4.js
@@ -0,0 +1,14 @@
+(function(){let e=document.createElement(`link`).relList;if(e&&e.supports&&e.supports(`modulepreload`))return;for(let e of document.querySelectorAll(`link[rel="modulepreload"]`))n(e);new MutationObserver(e=>{for(let t of e)if(t.type===`childList`)for(let e of t.addedNodes)e.tagName===`LINK`&&e.rel===`modulepreload`&&n(e)}).observe(document,{childList:!0,subtree:!0});function t(e){let t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin===`use-credentials`?t.credentials=`include`:e.crossOrigin===`anonymous`?t.credentials=`omit`:t.credentials=`same-origin`,t}function n(e){if(e.ep)return;e.ep=!0;let n=t(e);fetch(e.href,n)}})();var e=class extends HTMLElement{connectedCallback(){let e=this.getAttribute(`color`)||`currentColor`;this.classList.add(`absolute`,`bottom-0`,`left-0`,`w-full`,`overflow-hidden`,`leading-none`,`z-20`,`pointer-events-none`,`h-[60px]`,`md:h-[120px]`,`lg:h-[150px]`),this.innerHTML=`
+      <!-- Wave 1 (Back Layer - Slowest) -->
+      <svg class="absolute bottom-0 left-0 w-[200%] h-full animate-wave-slow opacity-25" viewBox="0 0 2400 120" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M 0 70 Q 300 45, 600 70 T 1200 70 T 1800 70 T 2400 70 L 2400 120 L 0 120 Z" fill="${e}"></path>
+      </svg>
+      <!-- Wave 2 (Middle Layer - Medium Speed) -->
+      <svg class="absolute bottom-0 left-0 w-[200%] h-full animate-wave-medium opacity-45" viewBox="0 0 2400 120" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M 0 60 Q 300 35, 600 60 T 1200 60 T 1800 60 T 2400 60 L 2400 120 L 0 120 Z" fill="${e}"></path>
+      </svg>
+      <!-- Wave 3 (Front Layer - Fast) -->
+      <svg class="absolute bottom-0 left-0 w-[200%] h-full animate-wave-fast" viewBox="0 0 2400 120" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M 0 50 Q 300 20, 600 50 T 1200 50 T 1800 50 T 2400 50 L 2400 120 L 0 120 Z" fill="${e}"></path>
+      </svg>
+    `}};customElements.define(`moving-waves`,e);
