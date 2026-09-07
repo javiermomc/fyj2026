dcc52a6cf73c584f41b9cbfce70b64c39260eea2
javiermomc <javiermomc@outlook.com>
2026-09-07 11:36:15 -0600

    Update description

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ import './common/SectionDividers/SectionDividers'
 const hotelRecommendations: Recommendation[] = [
 	{
 		name: 'Flamingo Inn',
-		description: 'Hotel con edificios tipo era colonial. Céntrico, cómodo y de fácil acceso a los lugares de la boda.',
+		description: 'Hotel con edificios tipo campestre. Céntrico, cómodo y de fácil acceso a los lugares de la boda.',
 		backgroundUrl: '/assets/hotels/flamingo-inn.jpg',
 		googleMapsUrl: 'https://maps.app.goo.gl/6MAaGhGkxyjNfAqF8',
 		appleMapsUrl: 'https://maps.apple/p/F9HChcPP0xf4Dp',
